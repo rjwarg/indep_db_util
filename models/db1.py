9662a8e5365c236e9d7165fb95b94a5ebae53b1b
root <root@intranet.ccpoa.org>
2015-01-19 08:14:08 -0800

    first commit of this application

---
+++ models/db1.py
@@ -0,0 +1,78 @@
+# coding: utf8
+db.define_table('mclient',
+                Field('last_name', 'string'),
+                Field('first_name','string'),
+                Field('minst', 'string'),
+                Field('address', 'string'),
+                Field('zip', 'string'),
+                Field('member_id', 'integer'),
+                Field('stat','string'),
+ #               auth.signature
+                primarykey = ['member_id'],
+                migrate = False
+                )
+
+db.define_table('case_action_master',
+                Field('action_name'),
+                Field('action_value'),
+ #               auth.signature,
+                primarykey = ['action_name'],
+                migrate = False
+)
+
+
+
+db.define_table('case_master',
+                Field('case_number'),
+                Field('member_id', 'reference mclient', ondelete='NO ACTION'),
+                Field('description','text'),
+                Field('date_assigned', 'date'),
+                Field('date_closed','date'),
+                Field('dead_file_box_number'),
+                Field('case_disposition', 'text'),
+                Field('assigned_to', 'reference auth_user', ondelete='NO ACTION'),
+               
+#                auth.signature,
+                
+                primarykey = ['case_number'],
+                migrate = False
+ )
+
+db.define_table('case_action',
+                Field('case_id', 'reference case_master'),
+                Field('action_id', 'reference case_action_master'),
+                Field('date_performed', 'date'),
+                Field('remarks', 'text'),
+ #               auth.signature,
+                primarykey = ['case_id','action_id'],
+                migrate = False
+ )
+
+dbx.define_table('casemasterxport',
+                Field('case_number', 'string'),
+                Field('member_id', 'integer'),
+                Field('lastname'),
+                Field('firstname'),
+                Field('institution'),
+                Field('casedescription', 'text'),
+                Field('dateassigned', 'date'),
+                Field('dateclosed', 'date'),
+                Field('deadfileboxnumber'),
+                Field('memberstatus'),
+                Field('casedisposition'),
+                migrate= False,
+                primarykey=['case_number']
+                )
+                
+db2_prod.define_table('member',
+                 Field('id_no','integer'),
+                 Field('name', 'string','length=21'),
+                 Field('first_name', 'string', 'length=21'),
+                 Field('minst', 'string', 'length=7'),
+                 Field('address','string', 'length=35'),
+                 Field('zip','string', 'length=10'),
+                 Field('phone','string', 'length=13'),
+                 Field('email','string', 'length= 50'),
+                 Field('stat','string','length=3'),
+                 primarykey = ['id_no'],
+                 migrate = False)
---
+++ modules/akey.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf8
+from gluon import *
+
+def get_ifxpw():
+    return 'vap--or'
+
+def get_mssqlpw():
+    return 'xiiu12iphe'
